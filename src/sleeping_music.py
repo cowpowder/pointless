41e1cca63e5bc6c89dd10ebc261d4860dc60f9a7
cowpowder <iamdesmond@Live.com.sg>
2023-07-13 21:36:23 +0800

    changed function name

--- src/sleeping_music.py
+++ src/sleeping_music.py
@@ -1,14 +1,15 @@
 import sys
-from src.scripts.sleeping_music import play
+from src.utils.functions import play_youtube
 
 if __name__ == "__main__":
 
     command = sys.argv[0]
 
     if len(sys.argv) >= 3 and sys.argv[1] == '--time' or  sys.argv[1] == '-t':
+        print(sys.argv)
         some_time = sys.argv[2]
         print(f"Setting youtube autoplay 'sleeping music' at {some_time} everyday.")
-        play(some_time)
+        play_youtube(some_time)
         
     else:
         print("Invalid parameter, try -t or --time")
